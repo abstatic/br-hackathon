1935c97d337b09449d32e3a2a65ddf96e5ed92e1
Abhishek Shrivastava <abhishek.shrivastava@bloomreach.com>
2018-07-18 12:59:40 +0530

    employee models added

--- qol/jupiter/models.py
+++ qol/jupiter/models.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    e_id = models.CharField(primary_key=True)
+    e_name = models.CharField(max_length=30)
+    e_type = models.CharField(max_lenth=15)
+
+    def __str__(self):
+        return self.e_name
+
+class TimeRecord(models.Model):
+    in_time = models.DateTimeField()
+    out_time = models.DateTimeField()
+    e_id = Models.ForeignKey(Employee, on_delete=Models.CASCADE)
