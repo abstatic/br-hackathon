f43fe9d60efb01f1f6d42056ea461f37bce3bc9c
Abhishek Shrivastava <abhishek.shrivastava@bloomreach.com>
2018-07-18 13:03:03 +0530

    updated timerecord model

--- qol/jupiter/models.py
+++ qol/jupiter/models.py
@@ -8,11 +8,10 @@ class Employee(models.Model):
     e_id = models.CharField(primary_key=True)
     e_name = models.CharField(max_length=30)
     e_type = models.CharField(max_lenth=15)
-
     def __str__(self):
         return self.e_name
 
 class TimeRecord(models.Model):
-    in_time = models.DateTimeField()
-    out_time = models.DateTimeField()
+    # TODO add choice field for the time 
+    time_record = models.DateTimeField()
     e_id = Models.ForeignKey(Employee, on_delete=Models.CASCADE)
