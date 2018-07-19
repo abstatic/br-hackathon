8c901b7879d736efa929564195b115b82058fbf7
Abhishek Shrivastava <abhishek.shrivastava@bloomreach.com>
2018-07-19 11:06:21 +0530

    model updates

--- qol/jupiter/models.py
+++ qol/jupiter/models.py
@@ -2,17 +2,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class Employee(models.Model):
-    e_id = models.CharField(primary_key=True)
+    e_id = models.CharField(primary_key=True, max_length=10)
     e_name = models.CharField(max_length=30)
-    e_type = models.CharField(max_lenth=15)
+    e_type = models.CharField(max_length=15)
     def __str__(self):
         return self.e_name
 
 class TimeRecord(models.Model):
     RECORD_TYPES = (('IN', 'In'), ('OUT', 'Out'))
-    time_names = models.CharField(max_lenth=5, choices=RECORD_TYPES)
+    time_names = models.CharField(max_length=5, choices=RECORD_TYPES)
     time_record = models.DateTimeField()
-    e_id = Models.ForeignKey(Employee, on_delete=Models.CASCADE)
+    e_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+admin.site.register(Employee)
+admin.site.register(TimeRecord)
